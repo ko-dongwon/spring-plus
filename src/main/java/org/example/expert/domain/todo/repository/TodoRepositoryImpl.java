f76c79fcaca8e6249e2db4cbc727b437bc2bbe04
kodongwon <dw9706@gmail.com>
2025-06-28 01:58:47 +0900

    8. QueryDSL

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,31 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.example.expert.domain.todo.entity.Todo;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.example.expert.domain.user.entity.QUser.user;
+
+public class TodoRepositoryImpl implements TodoRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    public TodoRepositoryImpl(EntityManager em){
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        Todo result = queryFactory
+                .selectFrom(todo)
+                .leftJoin(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+
+    }
+}
